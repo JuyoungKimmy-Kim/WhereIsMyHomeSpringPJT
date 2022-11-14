4d65a0b7cc4a13ff74574fce8aefa56acdbea649
Juyoung Kim <kimmykim.jy@gmail.com>
2022-11-14 21:58:48 +0900

    # 삽질중

---
+++ demo2/src/components/NavBar.vue
@@ -0,0 +1,125 @@
+<template>
+  <!-- Navbar Transparent -->
+  <nav
+    class="navbar navbar-expand-lg position-absolute top-0 z-index-3 w-100 shadow-none my-3 navbar-transparent"
+  >
+    <div class="container">
+      <a
+        class="navbar-brand text-white"
+        rel="tooltip"
+        title="Designed and Coded by Creative Tim"
+        data-placement="bottom"
+      >
+        <router-link to="/">Home</router-link></a
+      >
+      <button
+        class="navbar-toggler shadow-none ms-2"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navigation"
+        aria-controls="navigation"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon mt-2">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </span>
+      </button>
+      <div
+        class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0 ms-lg-12 ps-lg-5"
+        id="navigation"
+      >
+        <ul class="navbar-nav navbar-nav-hover ms-auto">
+          <li class="nav-item dropdown dropdown-hover mx-2 ms-lg-6">
+            <a
+              class="nav-link ps-2 d-flex cursor-pointer align-items-center"
+              id="dropdownMenuPages2"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i class="material-icons opacity-6 me-2 text-md">dashboard</i>
+              Pages
+              <img
+                src="../assets/img/down-arrow-white.svg"
+                alt="down-arrow"
+                class="arrow ms-auto ms-md-2 d-lg-block d-none"
+              />
+              <img
+                src="../assets/img/down-arrow-dark.svg"
+                alt="down-arrow"
+                class="arrow ms-auto ms-md-2 d-lg-none d-block"
+              />
+            </a>
+          </li>
+
+          <li class="nav-item dropdown dropdown-hover mx-2">
+            <a
+              class="nav-link ps-2 d-flex cursor-pointer align-items-center"
+              id="dropdownMenuBlocks"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i class="material-icons opacity-6 me-2 text-md">view_day</i>
+              Sections
+              <img
+                src="../assets/img/down-arrow-white.svg"
+                alt="down-arrow"
+                class="arrow ms-auto ms-md-2 d-lg-block d-none"
+              />
+              <img
+                src="../assets/img/down-arrow-dark.svg"
+                alt="down-arrow"
+                class="arrow ms-auto ms-md-2 d-lg-none d-block"
+              />
+            </a>
+          </li>
+
+          <li class="nav-item dropdown dropdown-hover mx-2">
+            <a
+              class="nav-link ps-2 d-flex cursor-pointer align-items-center"
+              id="dropdownMenuDocs"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <i class="material-icons opacity-6 me-2 text-md">article</i>
+              Docs
+              <img
+                src="../assets/img/down-arrow-white.svg"
+                alt="down-arrow"
+                class="arrow ms-auto ms-md-2 d-lg-block d-none"
+              />
+              <img
+                src="../assets/img/down-arrow-dark.svg"
+                alt="down-arrow"
+                class="arrow ms-auto ms-md-2 d-lg-none d-block"
+              />
+            </a>
+          </li>
+
+          <li class="nav-link ps-2 d-flex cursor-pointer align-items-center">
+            <router-link to="/map">Map</router-link>
+          </li>
+          <li class="nav-link ps-2 d-flex cursor-pointer align-items-center">
+            <router-link to="/register">Register</router-link>
+          </li>
+          <li class="nav-link ps-2 d-flex cursor-pointer align-items-center">
+            <router-link to="/login">Login</router-link>
+          </li>
+          <li class="nav-link ps-2 d-flex cursor-pointer align-items-center">
+            <router-link to="/contact">Contact</router-link>
+          </li>
+          <li class="nav-link ps-2 d-flex cursor-pointer align-items-center">
+            <router-link to="/search">Search</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+</template>
+
+<script></script>
+
+<style scoped></style>
---
+++ demo2/src/components/RegisterForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <section id="registerForm">
+    <div class="container py-4">
+      <div class="row">
+        <div class="col-lg-7 mx-auto d-flex justify-content-center flex-column">
+          <h3 class="text-center">회원가입</h3>
+          <br><br>
+          <form role="form" id="contact-form" method="post" autocomplete="off">
+            <div class="card-body">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="input-group input-group-static mb-4">
+                    <label>이름</label>
+                    <input class="form-control" placeholder="eg. 홍길동" type="text" >
+                  </div>
+                </div>
+
+
+
+                <div class="col-md-6 ps-2">
+                  <div class="input-group input-group-static mb-4">
+                    <label>생년월일</label>
+                    <input class="form-control" placeholder="eg. YYYYMMDD" type="text" >
+                  </div>
+                </div>
+              </div>
+              <div class="mb-4">
+                  <div class="input-group input-group-static mb-4">
+                    <label>이메일</label>
+                    <input class="form-control" placeholder="eg. hong@gildong.com" type="email" >
+                  </div>
+              </div>
+              <div class="mb-4">
+                  <div class="input-group input-group-static mb-4">
+                    <label>비밀번호</label>
+                    <input class="form-control" placeholder="최소 8자리 이상 : 영어 대문자, 소문자, 숫자, 특수문자 중 3종류 조합 " type="password" >
+                  </div>
+              </div>
+              <div class="mb-4">
+                  <div class="input-group input-group-static mb-4">
+                    <label>비밀번호 확인</label>
+                    <input class="form-control" placeholder="" type="password" >
+                  </div>
+              </div>
+              <div class="input-group mb-4 input-group-static">
+                <label>관심 지역을 1개 이상 선택해주세요</label>
+                <textarea name="message" class="form-control" id="message" rows="4">여기서 관심 지역 체크박스 형태? 로 선택하게 해서 
+                  아무 선택 없이 (시, 구군, 동) map 화면으로 들어갔을 때, 관심 지역을 먼저 보여주게 하는 거 어때요?
+                </textarea>
+              </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="form-check form-switch mb-4 d-flex align-items-center">
+                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" checked="" />
+                    <label class="form-check-label ms-3 mb-0" for="flexSwitchCheckDefault">
+                      <a href="javascript:;" class="text-dark"><u>이용약관</u></a
+                      >에 동의합니다.</label
+                    >
+                  </div>
+                </div>
+                <div class="col-md-12">
+                  <button type="submit" class="btn bg-gradient-dark w-100">가입</button>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#registerForm {
+  padding-top: 100px;
+}
+</style>
