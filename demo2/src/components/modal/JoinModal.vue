e9360b28b1515fd3bea8ace1e3ed24131f2c5e5d
Juyoung Kim <kimmykim.jy@gmail.com>
2022-11-17 00:43:31 +0900

    logo 추가 + ,updateModal 수정중

--- demo2/src/components/modal/JoinModal.vue
+++ demo2/src/components/modal/JoinModal.vue
@@ -14,19 +14,19 @@
                 <form role="form text-start">
                 <div class="input-group input-group-static mb-4">
                     <label>이름</label>
-                    <input type="text" class="form-control in" placeholder="홍길동">
+                    <input type="text" class="form-control in" v-model="userName" placeholder="홍길동">
                 </div>
                 <div class="input-group input-group-static mb-4">
                     <label>이메일</label>
-                    <input type="email" class="form-control" placeholder="john@email.com">
+                    <input type="email" class="form-control" v-model="userEmail" placeholder="john@email.com">
                 </div>
                 <div class="input-group input-group-static mb-4">
                     <label>비밀번호</label>
-                    <input type="password" class="form-control" placeholder="•••••••••••••">
+                    <input type="password" class="form-control" v-model="userPassword" placeholder="•••••••••••••">
                 </div>
                 <div class="input-group input-group-static mb-4">
                     <label>비밀번호 확인</label>
-                    <input type="password" class="form-control" placeholder="•••••••••••••">
+                    <input type="password" class="form-control" v-model="userPassword2" placeholder="•••••••••••••">
                 </div>
                 <div class="input-group input-group-static mb-4">
                     <label class="typo__label">관심지역</label>
@@ -51,10 +51,16 @@ import Multiselect from 'vue-multiselect'
 
 export default {
   components: {
-    Multiselect
+    Multiselect,
   },
   data () {
     return {
+      // userId='',
+      // userEmail='',
+      // userPassword='',
+      // userPassword2='',
+      valid: false,
+
       options: [
         {
           language: '시도',
@@ -80,6 +86,44 @@ export default {
       ],
       value: []
     }
+  },
+  methods : {
+    // 길이가 3 이상이면 이름 valid
+    validateUserName(userName) {
+        if (userName.length >= 3) return true;
+        else return false;
+    },
+    // 하나 이상의 숫자, 알파벳, 특수문자 포함
+    validatePassword(userPassword) {
+        var patternEngAtListOne = new RegExp(/[a-zA-Z]+/); // + for at least one
+        var patternSpeAtListOne = new RegExp(/[~!@#$%^&*()_+|<>?:{}]+/); // + for at least one
+        var patternNumAtListOne = new RegExp(/[0-9]+/); // + for at least one
+
+        if (
+          patternEngAtListOne.test(userPassword) &&
+          patternSpeAtListOne.test(userPassword) &&
+          patternNumAtListOne.test(userPassword) &&
+          userPassword.length >= 8
+        ) {
+          return true;
+        } else return false;
+    },
+    validatePassword2(userPassword2) {
+        if (userPassword2 == document.querySelector("#userPassword").value)
+          return true;
+        else return false;
+    },
+    validateUserEmail(userEmail) {
+        // ^ 시작일치, $ 끝 일치
+        // {2, 3} 2개 ~ 3개
+        // * 0회 이상, + 1회 이상
+        // [-_.] - 또는 _ 또는 .
+        // ? 없거나 1회
+        let regexp =
+          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+        if (regexp.test(userEmail)) return true;
+        else return false;
+      },
   }
 }
 </script>
