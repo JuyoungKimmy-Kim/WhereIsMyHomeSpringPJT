f9422b32c18a89e1cef220acd8d0a33ef081d7be
DESKTOP-KVCQHCD\SSAFY <kimmykim.jy@gmail.com>
2022-11-17 16:08:45 +0900

    Join Modal + WishList + UpdateModal

---
+++ demo2/src/components/board/WishList.vue
@@ -0,0 +1,171 @@
+<template>
+<div>
+    <header-page></header-page>
+
+    <div class="card card-body sticky blur shadow-blur mx-3 mx-md-6 mt-n6">
+        <section>
+        <div class="container">
+            <h4 class="row justify-content-center">찜목록 관리</h4>
+            <div class="row justify-content-center pb-5">
+            <div class="col-lg-11">
+                <div class="card">
+                <div class="table-responsive" style="overflow-x: hidden">
+                    <table class="table align-items-center mb-0">
+                    <thead>
+                        <tr>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">매물</th>
+                        <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">가격</th>
+                        <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">지도에서 확인하기</th>
+                        <th class="text-secondary text-xxs opacity-7">목록에서 삭제</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <td>
+                            <div class="d-flex px-2 py-1">
+                            <div>
+                                <img src="../../assets/img/apt/송정삼정그린코아더시티.jpg" class="avatar avatar-sm me-3">
+                            </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <h6 class="mb-0 text-xs">부산광역시 강서구 송정동</h6>
+                                <p class="text-xs text-secondary mb-0">송정삼정그린코아 더시티 오피스텔</p>
+                            </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="text-xs font-weight-bold mb-0">단위 (만원)</p>
+                            <p class="text-xs text-secondary mb-0">6,000</p>
+                        </td>
+                        <td class="align-middle text-center">
+                            <!--<span class="text-secondary text-xs font-weight-bold">23/04/18</span>-->
+                            <i class="material-icons opacity-6 me-2 text-md"> <router-link to="/map">
+                                    arrow_forward</router-link></i>
+                        </td>
+                        <td class="align-middle">
+                            <i class="material-icons opacity-6 me-2 text-md">close</i>
+                        </td>
+                        </tr>
+                        <tr>
+                        <td>
+                            <div class="d-flex px-2 py-1">
+                            <div>
+                                <img src="../../assets/img/apt/송정삼정그린코아더시티.jpg" class="avatar avatar-sm me-3">
+                            </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <h6 class="mb-0 text-xs">부산광역시 강서구 송정동</h6>
+                                <p class="text-xs text-secondary mb-0">송정삼정그린코아 더시티 오피스텔</p>
+                            </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="text-xs font-weight-bold mb-0">단위 (만원)</p>
+                            <p class="text-xs text-secondary mb-0">6,000</p>
+                        </td>
+                        <td class="align-middle text-center">
+                            <!--<span class="text-secondary text-xs font-weight-bold">23/04/18</span>-->
+                            <i class="material-icons opacity-6 me-2 text-md"> <router-link to="/map">
+                                    arrow_forward</router-link></i>
+                        </td>
+                        <td class="align-middle">
+                            <i class="material-icons opacity-6 me-2 text-md">close</i>
+                        </td>
+                        </tr>
+                                                <tr>
+                        <td>
+                            <div class="d-flex px-2 py-1">
+                            <div>
+                                <img src="../../assets/img/apt/송정삼정그린코아더시티.jpg" class="avatar avatar-sm me-3">
+                            </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <h6 class="mb-0 text-xs">부산광역시 강서구 송정동</h6>
+                                <p class="text-xs text-secondary mb-0">송정삼정그린코아 더시티 오피스텔</p>
+                            </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="text-xs font-weight-bold mb-0">단위 (만원)</p>
+                            <p class="text-xs text-secondary mb-0">6,000</p>
+                        </td>
+                        <td class="align-middle text-center">
+                            <!--<span class="text-secondary text-xs font-weight-bold">23/04/18</span>-->
+                            <i class="material-icons opacity-6 me-2 text-md"> <router-link to="/map">
+                                    arrow_forward</router-link></i>
+                        </td>
+                        <td class="align-middle">
+                            <i class="material-icons opacity-6 me-2 text-md">close</i>
+                        </td>
+                        </tr>
+                                                <tr>
+                        <td>
+                            <div class="d-flex px-2 py-1">
+                            <div>
+                                <img src="../../assets/img/apt/송정삼정그린코아더시티.jpg" class="avatar avatar-sm me-3">
+                            </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <h6 class="mb-0 text-xs">부산광역시 강서구 송정동</h6>
+                                <p class="text-xs text-secondary mb-0">송정삼정그린코아 더시티 오피스텔</p>
+                            </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="text-xs font-weight-bold mb-0">단위 (만원)</p>
+                            <p class="text-xs text-secondary mb-0">6,000</p>
+                        </td>
+                        <td class="align-middle text-center">
+                            <!--<span class="text-secondary text-xs font-weight-bold">23/04/18</span>-->
+                            <i class="material-icons opacity-6 me-2 text-md"> <router-link to="/map">
+                                    arrow_forward</router-link></i>
+                        </td>
+                        <td class="align-middle">
+                            <i class="material-icons opacity-6 me-2 text-md">close</i>
+                        </td>
+                        </tr>
+                                                <tr>
+                        <td>
+                            <div class="d-flex px-2 py-1">
+                            <div>
+                                <img src="../../assets/img/apt/송정삼정그린코아더시티.jpg" class="avatar avatar-sm me-3">
+                            </div>
+                            <div class="d-flex flex-column justify-content-center">
+                                <h6 class="mb-0 text-xs">부산광역시 강서구 송정동</h6>
+                                <p class="text-xs text-secondary mb-0">송정삼정그린코아 더시티 오피스텔</p>
+                            </div>
+                            </div>
+                        </td>
+                        <td>
+                            <p class="text-xs font-weight-bold mb-0">단위 (만원)</p>
+                            <p class="text-xs text-secondary mb-0">6,000</p>
+                        </td>
+                        <td class="align-middle text-center">
+                            <!--<span class="text-secondary text-xs font-weight-bold">23/04/18</span>-->
+                            <i class="material-icons opacity-6 me-2 text-md"> <router-link to="/map">
+                                    arrow_forward</router-link></i>
+                        </td>
+                        <td class="align-middle">
+                            <i class="material-icons opacity-6 me-2 text-md">close</i>
+                        </td>
+                        </tr>
+
+                    </tbody>
+                    </table>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </section>
+    </div>
+</div>
+</template>
+
+<script>
+import HeaderPage from '@/components/HeaderPage.vue'
+export default {
+    components:{
+        HeaderPage
+    }
+}
+</script>
+
+<style>
+</style>
--- demo2/src/components/modal/UpdateModal.vue
+++ demo2/src/components/modal/UpdateModal.vue
@@ -1,32 +1,51 @@
 <template>
-<div class="modal fade" id="joinModal" tabindex="-1"  aria-hidden="true">
+<div class="modal fade" id="updateModal" tabindex="-1"  aria-hidden="true">
 <div class="modal-dialog modal-danger modal-dialog-centered modal-" role="document">
     <div class="modal-content">
     <div class="modal-body p-0">
         <div class="card bg-gray-200 shadow border-0 mb-0">
             <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                 <div class="bg-gradient-info shadow-info border-radius-lg py-3 pe-1 text-center py-4">
-                <h4 class="font-weight-bolder text-white mt-1 mb-0">회원가입</h4>
-                <p class="mb-1 text-sm text-white">환영합니다.</p>
+                <h4 class="font-weight-bolder text-white mt-1 mb-0">회원 정보 수정</h4>
+                <p class="mb-1 text-sm text-white">환영합니다.</p> 
                 </div>
             </div>
+
+            <!-- 
+
+            <Vue - Session 사용 방법>
+
+            npm install -save uve-session 후
+            <main.js>
+                import VueSession from 'vue-session'
+                var sessionOptions = { persist : true}
+                Vue.use(VueSession. sessionOptions)
+
+            <setter>
+                this.$session.set('user_no', 'res.user_no')
+            <getter>
+                this.$session.get('user_no')
+
+            
+            -->
+
             <div class="card-body">
                 <form role="form text-start">
                 <div class="input-group input-group-static mb-4">
                     <label>이름</label>
-                    <input type="text" class="form-control in" v-model="userName" placeholder="홍길동">
+                    <input type="text" class="form-control in" placeholder="" disabled/>
                 </div>
                 <div class="input-group input-group-static mb-4">
                     <label>이메일</label>
-                    <input type="email" class="form-control" v-model="userEmail" placeholder="john@email.com">
+                    <input type="email" class="form-control" placeholder="" disabled/>
                 </div>
                 <div class="input-group input-group-static mb-4">
                     <label>비밀번호</label>
-                    <input type="password" class="form-control" v-model="userPassword" placeholder="•••••••••••••">
+                    <input type="password" class="form-control" placeholder="•••••••••••••">
                 </div>
                 <div class="input-group input-group-static mb-4">
                     <label>비밀번호 확인</label>
-                    <input type="password" class="form-control" v-model="userPassword2" placeholder="•••••••••••••">
+                    <input type="password" class="form-control" placeholder="•••••••••••••">
                 </div>
                 <div class="input-group input-group-static mb-4">
                     <label class="typo__label">관심지역</label>
@@ -35,7 +54,7 @@
                     </multiselect>
                 </div>
                 <div class="text-center">
-                    <button type="button" class="btn bg-gradient-info mt-4 mb-0">회원가입</button>
+                    <button type="button" class="btn bg-gradient-info mt-4 mb-0">수정완료</button>
                 </div>
                 </form>
             </div>
@@ -51,16 +70,10 @@ import Multiselect from 'vue-multiselect'
 
 export default {
   components: {
-    Multiselect,
+    Multiselect
   },
   data () {
     return {
-      // userId='',
-      // userEmail='',
-      // userPassword='',
-      // userPassword2='',
-      valid: false,
-
       options: [
         {
           language: '시도',
@@ -86,44 +99,6 @@ export default {
       ],
       value: []
     }
-  },
-  methods : {
-    // 길이가 3 이상이면 이름 valid
-    validateUserName(userName) {
-        if (userName.length >= 3) return true;
-        else return false;
-    },
-    // 하나 이상의 숫자, 알파벳, 특수문자 포함
-    validatePassword(userPassword) {
-        var patternEngAtListOne = new RegExp(/[a-zA-Z]+/); // + for at least one
-        var patternSpeAtListOne = new RegExp(/[~!@#$%^&*()_+|<>?:{}]+/); // + for at least one
-        var patternNumAtListOne = new RegExp(/[0-9]+/); // + for at least one
-
-        if (
-          patternEngAtListOne.test(userPassword) &&
-          patternSpeAtListOne.test(userPassword) &&
-          patternNumAtListOne.test(userPassword) &&
-          userPassword.length >= 8
-        ) {
-          return true;
-        } else return false;
-    },
-    validatePassword2(userPassword2) {
-        if (userPassword2 == document.querySelector("#userPassword").value)
-          return true;
-        else return false;
-    },
-    validateUserEmail(userEmail) {
-        // ^ 시작일치, $ 끝 일치
-        // {2, 3} 2개 ~ 3개
-        // * 0회 이상, + 1회 이상
-        // [-_.] - 또는 _ 또는 .
-        // ? 없거나 1회
-        let regexp =
-          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-        if (regexp.test(userEmail)) return true;
-        else return false;
-      },
   }
 }
 </script>
