8b1898dd4ff78ad0bacdb46cd66b65870f2bbe9e
ddings <ddings7303@gmail.com>
2022-11-17 20:18:51 +0900

    잘못 올린거 수정
    작업 안된버전의 Vue를 올려서 Revert후 다시 커밋

---
+++ WhereIsMyHome_Final/src/main/java/com/mycom/myhome/user/UserServieImpl.java
@@ -0,0 +1,5 @@
+package com.mycom.myhome.user;
+
+public class UserServieImpl {
+
+}
---
+++ WhereIsMyHome_Final/src/main/java/com/mycom/myhome/user/UserVo.java
@@ -0,0 +1,11 @@
+package com.mycom.myhome.user;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class UserVo {
+	private String userEmail;
+	private String userPassword;
+}
