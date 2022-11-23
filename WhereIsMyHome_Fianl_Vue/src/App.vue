0e6c40c3940a108eb733d103b72066a08577f7a3
Juyoung Kim <kimmykim.jy@gmail.com>
2022-11-23 22:40:05 +0900

    나머지 Front 수정 사항

--- WhereIsMyHome_Fianl_Vue/src/App.vue
+++ WhereIsMyHome_Fianl_Vue/src/App.vue
@@ -5,50 +5,150 @@
     <router-view name="Footer"></router-view>
 
     
-    <login-modal @show-join="showJoinModal"></login-modal>
-    <join-modal></join-modal>
-    <update-modal></update-modal>
+    <login-modal @show-signUp="showSignUp" @close-this-modal="closeLogin" ref="login_modal"></login-modal>
+    <signup-modal @close-this-modal="closeSignUp" ref="signup_modal"></signup-modal>
+    <update-modal @close-this-modal="closeInfo" ref="update_modal"></update-modal>
 
+    <spinner-ui v-if="isLoading"></spinner-ui>
 
   </div>
 </template>
 
 <script>
+import http from "@/common/axios";
+
+
 import NavBar from "@/components/NavBar.vue"
+import SpinnerUi from '@/components/SpinnerUI.vue'
+
+import Chart from 'chart.js';
+import ChartData from '@/common/chart-data.js';
 
 import { Modal } from "bootstrap";
 import LoginModal from "@/components/modal/LoginModal.vue"
-import JoinModal from "@/components/modal/JoinModal.vue"
+import SignupModal from "@/components/modal/SignupModal.vue"
 import UpdateModal from "@/components/modal/user/UpdateUserInfo.vue"
+
+import { mapState, mapActions } from "vuex";
+
+const userStore = "userStore";
 
 export default {
   data() {
     return {
       loginModal: null,
-      joinModal: null,
+      signUpModal: null,
       updateModal: null,
+      ChartData,
     }
   },
-  components: { LoginModal, JoinModal, NavBar, UpdateModal },
+  components: { LoginModal, SignupModal, NavBar, UpdateModal, SpinnerUi},
+  computed:{
+    ...mapState(userStore, ["userInfo"]),
+    ...mapState(["isLoading"]),
+  },
   methods: {
+    ...mapActions(userStore, ["getUserInfo"]),
     showLogin(){
+      if(this.loginModal == null){
+        this.loginModal = new Modal(document.getElementById("loginModal"));
+      }
+
+      this.$refs.login_modal.user.email = "";
+      this.$refs.login_modal.user.password = "";
+
       this.loginModal.show();
     },
-    showJoinModal(){
+    showSignUp(){
       this.loginModal.hide();
-      this.joinModal.show();
+
+      if(this.signUpModal == null){
+        this.signUpModal = new Modal(document.getElementById("signUpModal"));
+      }
+
+      this.$refs.signup_modal.user = {
+        email: "",
+        name: "",
+        password: "",
+        password2: "",
+        profileImageUrl: "",
+      };
+
+
+      this.$refs.signup_modal.valid = {
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+      }
+
+      if(this.userInfo != {}){
+        this.$refs.signup_modal.user.email = this.userInfo.email;
+        this.$refs.signup_modal.user.name = this.userInfo.name;
+        this.$refs.signup_modal.user.profileImageUrl = this.userInfo.profileImageUrl;
+      }
+
+      this.signUpModal.show();
     },
-    showInfo() {
+    closeLogin(){
+      this.loginModal.hide();
+    },
+    closeSignUp(){
+      this.signUpModal.hide();
+    },
+    closeInfo(){
+      this.updateModal.hide();
+    },
+    async showInfo() {
+      if(this.updateModal == null){
+        this.updateModal= new Modal(document.getElementById("updateModal"));
+      }
+
+      this.$refs.update_modal.password = "";
+      this.$refs.update_modal.password2 = "";
+      this.$refs.update_modal.passwordColor = "";
+      this.$refs.update_modal.passwordColor2 = "";
+
       this.updateModal.show();
     }
   },
-  mounted() {
-      this.loginModal = new Modal(document.getElementById("loginModal"));
-      this.joinModal = new Modal(document.getElementById("joinModal"));
-      this.updateModal=new Modal(document.getElementById("updateModal"));
+  async mounted() {
+    let token = sessionStorage.getItem("access-token");
+    await this.getUserInfo(token);
   },
+  created(){
+      http.interceptors.request.use(
+          (config) => {
+              this.$store.commit("SET_IS_LOADING", true);
+              return config;
+          },
+          (error) => {
+              this.$store.commit("SET_IS_LOADING", false);
+              return Promise.reject(error);
+          }
+      );
+
+      http.interceptors.response.use(
+          (response) => {
+            this.$store.commit("SET_IS_LOADING", false);
+            return response;
+          },
+          (error) => {
+            this.$store.commit("SET_IS_LOADING", false);
+            return Promise.reject(error);
+          }
+      );
+  }
 }
 </script>
 
 <style>
+html{
+     /* 드래그 방지 */
+    -ms-user-select: none;
+    -moz-user-select: -moz-none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    user-select: none;
+}
 </style>
---
+++ WhereIsMyHome_Fianl_Vue/src/common/chart-data.js
@@ -0,0 +1,28 @@
+export default ({
+    type :'line',
+    data : {
+        lables : ['Jan', 'Feb', 'Mar', 'Apr', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        datasets : [
+            {
+                label : '월별 거래 금액 / 단위: 만원',
+                data : [],
+                backgroundColor : 'rgba(54,73,93,.5)',
+                borderColor : '#36495d',
+                borderWidth : 1,
+            },
+        ]
+    },
+
+    options : {
+        responsive : true,
+        lineTension :1,
+        scales : {
+            yAxes : [{
+                ticks : {
+                    beginAtZero :true,
+                    padding :25,
+                }
+            }]
+        },
+    }
+})
