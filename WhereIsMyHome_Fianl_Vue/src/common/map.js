1ce82558d1b676b219db51b12fb6213d70a2e81c
Juyoung Kim <kimmykim.jy@gmail.com>
2022-11-23 22:45:58 +0900

    Front - common/map.js 추가

---
+++ WhereIsMyHome_Fianl_Vue/src/common/map.js
@@ -0,0 +1,27 @@
+import http from "./axios.js"
+
+async function sidoList(success, fail){
+    await http.get("/address").then(success).catch(fail);
+}
+
+async function gugunList(sidoCode, success, fail){
+    await http.get(`/address/${sidoCode}`).then(success).catch(fail);
+}
+
+async function dongList(sidoCode, gugunCode, success, fail){
+    await http.get(`/address/${sidoCode}/${gugunCode}`).then(success).catch(fail);
+}
+
+async function propertyList(dongName, gugunName, success, fail){
+    await http.get(`/property/${dongName}/${gugunName}`).then(success).catch(fail);
+}
+
+async function propertyDetail(houseNo, success, fail) {
+    await http.get(`/propertyDetail/${houseNo}`).then(success).catch(fail);
+}
+
+async function stationList(success, fail) {
+    await http.get("/stations").then(success).catch(fail);
+}
+
+export {sidoList, gugunList, dongList, propertyList, stationList, propertyDetail}
